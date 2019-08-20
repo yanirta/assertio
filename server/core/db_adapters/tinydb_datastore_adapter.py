dfa4bb7e30578fb4847d27faca4740ae13d2aabb
Yanir <6667420+yanirta@users.noreply.github.com>
2019-08-20 18:56:54 +0300

    Adding concurrency resilience to server database adapter

--- server/core/db_adapters/tinydb_datastore_adapter.py
+++ server/core/db_adapters/tinydb_datastore_adapter.py
@@ -1,45 +1,66 @@
 from core.db_adapters.datastore_adapter_base import DatastoreAdapterBase
 from tinydb import TinyDB, Query
 from openapi_server.models.check import Check
+from threading import RLock
 
 
 class TinyDbDatastoreAdapter(DatastoreAdapterBase):
     db = None
     bases = None
     history = None
+    lock = RLock()
 
     def __init__(self, dbname='db.json'):
-        if(TinyDbDatastoreAdapter.db == None):
-            TinyDbDatastoreAdapter.db = TinyDB(dbname)
-        if(TinyDbDatastoreAdapter.bases == None):
-            TinyDbDatastoreAdapter.bases = TinyDbDatastoreAdapter.db.table(
-                'bases')
-        if(TinyDbDatastoreAdapter.history == None):
-            TinyDbDatastoreAdapter.history = TinyDbDatastoreAdapter.db.table(
-                'history')
+        try:
+            TinyDbDatastoreAdapter.lock.acquire()
+            if(TinyDbDatastoreAdapter.db == None):
+                TinyDbDatastoreAdapter.db = TinyDB(dbname)
+            if(TinyDbDatastoreAdapter.bases == None):
+                TinyDbDatastoreAdapter.bases = TinyDbDatastoreAdapter.db.table(
+                    'bases')
+            if(TinyDbDatastoreAdapter.history == None):
+                TinyDbDatastoreAdapter.history = TinyDbDatastoreAdapter.db.table(
+                    'history')
+        finally:
+            TinyDbDatastoreAdapter.lock.release()
 
     def insertCheck(self, check, result):
-        # Reformatting date time to be serializable
-        if(hasattr(check.timestamp, 'isoformat')):
-            check.timestamp = check.timestamp.isoformat()
-        return TinyDbDatastoreAdapter.history.insert({'check': check.to_dict(), 'result': result.to_dict()})
+        try:
+            TinyDbDatastoreAdapter.lock.acquire()
+            # Reformatting date time to be serializable
+            if(hasattr(check.timestamp, 'isoformat')):
+                check.timestamp = check.timestamp.isoformat()
+            return TinyDbDatastoreAdapter.history.insert({'check': check.to_dict(), 'result': result.to_dict()})
+        finally:
+            TinyDbDatastoreAdapter.lock.release()
 
     def getBase(self, uname, identifier):
-        Base = Query()
-        res = TinyDbDatastoreAdapter.bases.search(
-            (Base.uname == uname) & (Base.identifier == identifier))
-        if len(res) == 0:
-            return {}
-        elif len(res) == 1:
-            return res[0]['base']
-        else:
-            raise 'Got more than one result'
+        try:
+            TinyDbDatastoreAdapter.lock.acquire()
+            Base = Query()
+            res = TinyDbDatastoreAdapter.bases.search(Base.uname == uname)
+            # TBD & (Base.identifier == identifier))
+            if ((res == None) or len(res) == 0):
+                return {}
+            elif len(res) == 1:
+                return res[0]['base']
+            else:
+                raise 'Got more than one result'
+        except BaseException as e:
+            print(e)
+        finally:
+            TinyDbDatastoreAdapter.lock.release()
 
     def upsertBase(self, uname, identifier, base):
         Base = Query()
-        return TinyDbDatastoreAdapter.bases.upsert(
-            {'uname': uname, 'identifier': identifier, 'base': base},
-            (Base.uname == uname) & (Base.identifier == identifier))
+        try:
+            TinyDbDatastoreAdapter.lock.acquire()
+            return TinyDbDatastoreAdapter.bases.upsert(
+                {'uname': uname, 'identifier': identifier, 'base': base},
+                (Base.uname == uname))
+            # TBD & (Base.identifier == identifier))
+        finally:
+            TinyDbDatastoreAdapter.lock.release()
 
     def getCheck(self, docid):
         return self.history.get(doc_id=docid)
